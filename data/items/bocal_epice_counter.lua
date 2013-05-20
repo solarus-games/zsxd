b405243398a83410f83e68b195f9a9415dff571b
Christophe <christopho128@gmail.com>
2013-05-20 17:12:37 +0200

    Fix item scripts.

--- data/items/bocal_epice_counter.lua
+++ data/items/bocal_epice_counter.lua
@@ -2,8 +2,8 @@ local item = ...
 
 function item:on_created()
 
-  self:set_savegame_variable("1488")
-  self:set_amount_savegame_variable("1478")
+  self:set_savegame_variable("i1488")
+  self:set_amount_savegame_variable("i1478")
   self:set_max_amount(50)
 end
 
--- data/items/parfum_counter.lua
+++ data/items/parfum_counter.lua
@@ -3,7 +3,7 @@ local item = ...
 function item:on_created()
 
   self:set_savegame_variable("i1490")
-  self:set_amount_savegame_variable("1480")
+  self:set_amount_savegame_variable("i1480")
   self:set_max_amount(50)
 end
 
