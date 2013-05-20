fa11caa03830c69f226beb3c16689c221014d5cc
Christophe <christopho128@gmail.com>
2013-05-20 13:17:09 +0200

    Starting to update zsxd scripts to Solarus 1.0 format.

---
+++ data/menus/dialog_box.lua
@@ -0,0 +1,36 @@
+local game = ...
+
+local dialog_box = {}
+
+function dialog_box:new(game)
+
+  local object = { game = game }
+  setmetatable(object, self)
+  self.__index = self
+
+  return object
+end
+
+function dialog_box:on_draw(dst_surface)
+
+  -- The dialog box is actually still built-in for now.
+  local map = self.game:get_map()
+  if map ~= nil and map:is_dialog_enabled() then
+    map:draw_dialog_box(dst_surface)
+  end
+end
+
+function game:initialize_dialog_box()
+
+  self.dialog_box = dialog_box:new(self)
+  sol.menu.start(self, self.dialog_box)
+end
+
+function game:quit_dialog_box()
+
+  if self.dialog_box ~= nil then
+    sol.menu.stop(self.dialog_box)
+    self.dialog_box = nil
+  end
+end
+
