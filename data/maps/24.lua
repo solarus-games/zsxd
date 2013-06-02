4b7d946eeeac9edba700fc6b45748d17c7fca438
Christophe <christopho128@gmail.com>
2013-06-02 16:06:04 +0200

    Update the missing map to the new Lua API.

--- data/maps/24.lua
+++ data/maps/24.lua
@@ -1,307 +1,315 @@
+local map = ...
+local game = map:get_game()
+
 ----------------------------------
 -- Crazy House 3F               --
 ----------------------------------
 
 local giga_bouton_pushed = false
 local dialogue_trop_leger_fait = false
-local guichet33_sprite
 
-function event_map_started(destination_point_name)
+function map:on_started(destination_point)
 
   -- Desactivation des sensors pour la porte du couloir sans fin        
-  sol.map.sensor_set_enabled("bowser_leave", false)
-  sol.map.sensor_set_enabled("bowser_exit", false)
+  bowser_leave:set_enabled(false)
+  bowser_exit:set_enabled(false)
 
   -- Boutons sauvegardés
-  if sol.game.savegame_get_boolean(126) then
-    sol.map.sensor_set_enabled("infinite_corridor", false)
-    sol.map.switch_set_activated("giga_bouton", true)
+  if game:get_value("b126") then
+    infinite_corridor:set_enabled(false)
+    giga_bouton:set_activated(true)
   end
 
-  if sol.game.savegame_get_boolean(129) then
-    sol.map.tile_set_group_enabled("barrier", false)
-    sol.map.switch_set_activated("barrier_switch", true)
+  if game:get_value("b129") then
+    map:set_entities_enabled("barrier_tile", false)
+    barrier_switch:set_activated(true)
   end
-
-  guichet33_sprite = sol.map.npc_get_sprite("GC33")
 end
 
-function event_map_opening_transition_finished(destination_point_name)
+function map:on_opening_transition_finished(destination_point)
 
   -- Affichage du nom du donjon quand on vient de l'escalier de dehors
-  if destination_point_name == "fromOutsideSO" then
-    sol.map.dialog_start("crazy_house.title")
+  if destination_point == fromOutsideSO then
+    map:start_dialog("crazy_house.title")
   end
 end
 
 -- Guichet 31 -------------------------------------------------
-function guichet_31()
+local function guichet_31()
 
-  sol.map.dialog_start("crazy_house.guichet_31")
+  map:start_dialog("crazy_house.guichet_31")
 end
 
 -- Guichet 32 -------------------------------------------------
-function guichet_32()
+local function guichet_32()
 
-  if sol.game.savegame_get_integer(1410) == 6 then
-    sol.map.dialog_start("crazy_house.guichet_32_ech_le_6")
-    sol.game.savegame_set_integer(1410, 7)
+  if game:get_value("i1410") == 6 then
+    map:start_dialog("crazy_house.guichet_32_ech_le_6")
+    game:set_value("i1410", 7)
   else
-    sol.map.dialog_start("crazy_house.guichet_32_ech_ne_6")
+    map:start_dialog("crazy_house.guichet_32_ech_ne_6", function(answer)
+
+      -- Echange de hache contre cuillere
+      if answer == 0 then
+        if game:get_item("hache_counter"):has_amount(1) then
+          hero:start_treasure("cuillere")
+          game:get_item("hache_counter"):remove_amount(1)
+        else
+          sol.audio.play_sound("wrong")
+          map:start_dialog("crazy_house.guichet_32_ech_ne_6_un")
+        end
+      else
+        map:start_dialog("crazy_house.guichet_32_ech_ne_6_no")
+      end
+
+    end)
   end
   -- Incrémentation branche 1412
-  branche1412 = sol.game.savegame_get_integer(1412)
+  local branche1412 = game.get_value("i1412") or 0
   if branche1412 > 0 and branche1412 <= 4 then
-    sol.game.savegame_set_integer(1412, 5)
+    game:set_value("i1412", 5)
   end
 end
 
 -- Guichet 33 -------------------------------------------------
-function guichet_33()
+local function guichet_33()
 
-  small_key_obtained = sol.game.savegame_get_boolean(123)
-  if sol.game.savegame_get_integer(1410) == 3
+  local small_key_obtained = game:get_value("b123")
+  local branche1410 = game:get_value("i1410") or 0
+  if branche1410 == 3
       and not small_key_obtained then        
-    sol.map.dialog_start("crazy_house.guichet_33_ech_eq_3")
-  elseif sol.game.savegame_get_integer(1410) >= 4
+    map:start_dialog("crazy_house.guichet_33_ech_eq_3")
+  elseif branche1410 >= 4
       and not small_key_obtained then
-    if sol.game.get_item_amount("parfum_counter") >= 1 then
+    if game:get_item("parfum_counter"):has_amount(1) then
       -- A le parfum
-      sol.map.dialog_start("crazy_house.guichet_33_parfum")
+      map:start_dialog("crazy_house.guichet_33_parfum", function()
+
+        -- Obtention clé du guichet 33 suite à l'apport du parfum
+        if game:get_item("parfum_counter"):has_amount(1) then
+          hero:give_treasure("small_key", 1, "b123")
+          game:get_item("parfum_counter"):remove_amount(1)
+        end
+
+      end)
     else
       -- N'a pas encore le parfum
-      sol.map.dialog_start("crazy_house.guichet_33_ech_ge_4")
-      if sol.game.savegame_get_integer(1410) == 4 then
-        sol.game.savegame_set_integer(1410, 5)
+      map:start_dialog("crazy_house.guichet_33_ech_ge_4")
+      if branche1410 == 4 then
+        game:set_value("i1410", 5)
       end
     end
   else
-    sol.map.dialog_start("crazy_house.guichet_33_ech_le_2")
+    map:start_dialog("crazy_house.guichet_33_ech_le_2")
   end
 end
 
 -- Apothicaire ------------------------------------------------
-function apothicaire()
+local function apothicaire()
 
-  sol.map.dialog_start("crazy_house.apothicaire")
+  map:start_dialog("crazy_house.apothicaire", function(answer)
+
+    -- Achat de sacs de riz à l'apothicaire        	
+    if answer == 1 then
+      if game:get_money() >= 20 then
+        map:start_dialog("crazy_house.apothicaire_oui", function()
+
+          -- Remise du sac de riz achetés à l'apothicaire
+          hero:start_treasure("sac_riz")
+
+        end)
+        game:remove_money(20)
+
+        -- Incrémentation branche 1411
+        local branche1411 = game:get_value("i1411")
+        if branche1411 == 4 then
+          game:set_value("i1411", 5)
+        end
+
+        -- Incrémentation branche 1412
+        local branche1412 = game:get_value("i1412") or 0
+        if branche1412 >= 6 and branche1412 <= 7 then
+          game:set_value("i1412", 8)
+        end
+      else
+        sol.audio.play_sound("wrong")
+        map:start_dialog("crazy_house.apothicaire_rubis")
+      end
+    else
+      map:start_dialog("crazy_house.apothicaire_non")
+    end
+
+  end)
 end
 
 -- Guichet 36 -------------------------------------------------
-function guichet_36()
+local function guichet_36()
 
-  sol.map.dialog_start("crazy_house.guichet_36")
-end
+  map:start_dialog("crazy_house.guichet_36", function(answer)
 
-function event_npc_interaction(npc_name)
-
-  if npc_name == "GC33" then
-    guichet_33()
-  elseif npc_name == "Apothicaire" then
-    apothicaire()
-  elseif npc_name == "GC31" then
-    guichet_31()
-  elseif npc_name == "GC32" then
-    guichet_32()
-  elseif npc_name == "GC33Front" then
-    sol.main.sprite_set_direction(guichet33_sprite, 3)
-    guichet_33()
-  elseif npc_name == "GC36" then
-    guichet_36()
-  elseif npc_name == "Apothicaire" then
-    apothicaire()
-  end
-end
-
-function event_hero_on_sensor(sensor_name)
-
-  -- Link approche de la porte qui annonce le couloir sans fin        
-  if sensor_name == "bowser_message" then
-    if not sol.game.savegame_get_boolean(126) then
-      -- Bouton pas appuyé
-      sol.map.sensor_set_enabled("bowser_close", true)
-      if not sol.map.door_is_open("bowser_door") then
-        sol.map.dialog_start("crazy_house.infinite_greetings")
-        sol.main.play_sound("sm64_bowser_message")
+    -- Achat de 3 sacs de riz à Panoda Fichage
+    if answer == 1 then
+      if game:get_money() >= 50 then
+        game:remove_money(50)
+        hero:start_treasure("sac_riz")
+        game:get_item("sac_riz_counter"):add_amount(2)
+        -- Incrémentation branche 1411
+        local branche1411 = game:get_value("i1411")
+        if branche1411 == 4 then
+          game:set_value("i1411", 5)
+        end
+        -- Incrémentation branche 1412
+        local branche1412 = game:get_value("i1412")
+        if branche1412 >= 6 and branche1412 <= 7 then
+          game:set_value("i1412", 8)
+        end
       else
-        sol.map.sensor_set_enabled("bowser_leave", true)
+        sol.audio.play_sound("wrong")
+        map:start_dialog("crazy_house.guichet_36_un")
       end
+    end
+
+  end)
+end
+
+function GC33:on_interaction()
+  guichet_33()
+end
+
+function Apothicaire:on_interaction()
+  apothicaire()
+end
+
+function GC31:on_interaction()
+  guichet_31()
+end
+
+function GC32:on_interaction()
+  guichet_32()
+end
+
+function GC33Front:on_interaction()
+  GC33:get_sprite():set_direction(3)
+  guichet_33()
+end
+
+function GC36:on_interaction()
+  guichet_36()
+end
+
+function Apothicaire:on_interaction()
+  apothicaire()
+end
+
+-- Link approche de la porte qui annonce le couloir sans fin        
+function bowser_message:on_activated()
+
+  if not game.get_value("b126") then
+    -- Bouton pas appuyé
+    bowser_close:set_enabled(true)
+    if bowser_door:is_closed() then
+      map:start_dialog("crazy_house.infinite_greetings", function()
+
+        -- Ouverture de la porte vers le couloir sans fin
+        if bowser_door:is_closed() then
+          map:open_doors("bowser_door")
+        end
+        bowser_leave:set_enabled(true)
+
+      end)
+      sol.audio.play_sound("sm64_bowser_message")
     else
-      -- Bouton déjà appuyé
-      if not sol.map.door_is_open("bowser_door") then
-        sol.map.door_open("bowser_door")
-      end
-      sol.map.sensor_set_enabled("bowser_leave", true)
-    end        
-    -- Fermeture de la porte derrière Link après être entré
-  elseif sensor_name == "bowser_close" then
-    if sol.map.door_is_open("bowser_door") then
-      sol.map.door_close("bowser_door")
+      bowser_leave:set_enabled(true)
     end
-    sol.map.sensor_set_enabled("bowser_exit", true)
-    sol.map.sensor_set_enabled("bowser_close", false)
-    -- Ouverture de la porte si Link souhaite sortir
-  elseif sensor_name == "bowser_exit" then
-    if not sol.map.door_is_open("bowser_door") then
-      sol.map.door_open("bowser_door")
+  else
+    -- Bouton déjà appuyé
+    if bowser_door:is_closed() then
+      map:open_doors("bowser_door")
     end
-    sol.map.sensor_set_enabled("bowser_message", false)
-    sol.map.sensor_set_enabled("bowser_close", true)
-    sol.map.sensor_set_enabled("bowser_exit", false)
-    -- Fermeture de la porte derrière Link après être sorti
-  elseif sensor_name == "bowser_leave" then
-    if sol.map.door_is_open("bowser_door") then
-      sol.map.door_close("bowser_door")
-    end
-    sol.map.sensor_set_enabled("bowser_message", true)
-    sol.map.sensor_set_enabled("bowser_leave", false)
-    -- Mécanisme du couloir sans fin
-  elseif sensor_name == "infinite_corridor" then
-    sol.map.hero_set_position(1136, 797, 1)
-    -- Giga bouton : Link trop léger
-  elseif sensor_name == "giga_bouton_sensor"
-      and not dialogue_trop_leger_fait
-      and not sol.map.switch_is_activated("giga_bouton") then
-    sol.map.dialog_start("crazy_house.giga_bouton_trop_leger")
+    bowser_leave:set_enabled(true)
+  end        
+end
+
+-- Fermeture de la porte derrière Link après être entré
+function bowser_close:on_activated()
+
+  if bowser_door:is_open() then
+    map:close_doors("bowser_door")
+  end
+  bowser_exit:set_enabled(true)
+  bowser_close:set_enabled(false)
+end
+
+-- Ouverture de la porte si Link souhaite sortir
+function bowser_exit:on_activated()
+
+  if bowser_door:is_closed() then
+    map:open_doors("bowser_door")
+  end
+  bowser_message:set_enabled(false)
+  bowser_close:set_enabled(true)
+  bowser_exit:set_enabled(false)
+end
+
+-- Fermeture de la porte derrière Link après être sorti
+function bowser_leave:on_activated()
+
+  if bowser_door:is_open() then
+    map:close_doors("bowser_door")
+  end
+  bowser_message:set_enabled(true)
+  bowser_leave:set_enabled(false)
+end
+
+-- Mécanisme du couloir sans fin
+function infinite_corridor:on_activated()
+  hero:set_position(1136, 797, 1)
+end
+
+-- Giga bouton : Link trop léger
+function giga_bouton_sensor:on_activated()
+
+  if not dialogue_trop_leger_fait
+      and not giga_bouton:is_activated() then
+    map:start_dialog("crazy_house.giga_bouton_trop_leger")
     dialogue_trop_leger_fait = true
   end
 end
 
-function event_dialog_finished(dialog_id, answer)
+-- Appui sur le giga bouton d'extra collision à charge supérieure de
+-- 15.000 megawatts
+function GBS:on_moved()
 
-  if dialog_id == "crazy_house.infinite_greetings" then
-    -- Ouverture de la porte vers le couloir sans fin
-    if not sol.map.door_is_open("bowser_door") then
-      sol.map.door_open("bowser_door")
-    end
-    sol.map.sensor_set_enabled("bowser_leave", true)
-  elseif dialog_id == "crazy_house.guichet_33_parfum" then
-    -- Obtention clé du guichet 33 suite à l'apport du parfum
-    if sol.game.get_item_amount("parfum_counter") > 0 then
-      sol.map.treasure_give("small_key", 1, 123)
-      sol.game.remove_item_amount("parfum_counter", 1)
-    end
-  elseif dialog_id == "crazy_house.apothicaire" then
-    -- Achat de sacs de riz à l'apothicaire        	
-    if answer == 0 then
-      if sol.game.get_money() >= 20 then
-        sol.map.dialog_start("crazy_house.apothicaire_oui")
-        sol.game.remove_money(20)
-
-        -- Incrémentation branche 1411
-        local branche1411 = sol.game.savegame_get_integer(1411)
-        if branche1411 == 4 then
-          sol.game.savegame_set_integer(1411, 5)
-        end
-
-        -- Incrémentation branche 1412
-        local branche1412 = sol.game.savegame_get_integer(1412)
-        if branche1412 >= 6 and branche1412 <= 7 then
-          sol.game.savegame_set_integer(1412, 8)
-        end
-      else
-        sol.main.play_sound("wrong")
-        sol.map.dialog_start("crazy_house.apothicaire_rubis")
-      end
-    else
-      sol.map.dialog_start("crazy_house.apothicaire_non")
-    end
-  elseif dialog_id == "crazy_house.apothicaire_oui" then
-    -- Remise du sac de riz achetés à l'apothicaire
-    sol.map.treasure_give("sac_riz", 1, -1)
-  elseif dialog_id == "crazy_house.guichet_36" then
-    -- Achat de 3 sacs de riz à Panoda Fichage
-    if answer == 0 then
-      if sol.game.get_money() >= 50 then
-        sol.game.remove_money(50)
-        sol.map.treasure_give("sac_riz", 1, -1)
-        sol.game.add_item_amount("sac_riz_counter", 2)
-        -- Incrémentation branche 1411
-        local branche1411 = sol.game.savegame_get_integer(1411)
-        if branche1411 == 4 then
-          sol.game.savegame_set_integer(1411, 5)
-        end
-        -- Incrémentation branche 1412
-        local branche1412 = sol.game.savegame_get_integer(1412)
-        if branche1412 >= 6 and branche1412 <= 7 then
-          sol.game.savegame_set_integer(1412, 8)
-        end
-      else
-        sol.main.play_sound("wrong")
-        sol.map.dialog_start("crazy_house.guichet_36_un")
-      end
-    end
-  elseif dialog_id == "crazy_house.guichet_32_ech_ne_6" then
-    -- Echange de hache contre cuillere
-    if answer == 0 then
-      if sol.game.get_item_amount("hache_counter") >= 1 then
-        sol.map.treasure_give("cuillere", 1, -1)
-        sol.game.remove_item_amount("hache_counter", 1)
-      else
-        sol.main.play_sound("wrong")
-        sol.map.dialog_start("crazy_house.guichet_32_ech_ne_6_un")
-      end
-    else
-      sol.map.dialog_start("crazy_house.guichet_32_ech_ne_6_no")
+  if not giga_bouton_pushed then
+    local x, y = self:get_position()
+    if x == 1664 and y == 797 then
+      giga_bouton_pushed = true
+      giga_bouton:set_activated(true)
+      infinite_corridor:set_enabled(false)        
+      game:set_value("b126", true)
+      map:move_camera(888, 792, 250, function()
+        sol.audio.play_sound("secret")
+      end)
+      infinite_corridor:set_enabled(false)
     end
   end
 end
 
-function event_block_moved(block_name)
-
-  if giga_bouton_pushed == false then
-    if block_name == "GBS" then        
-      local x, y = sol.map.block_get_position(block_name)
-      if x == 1664 and y == 797 then
-        giga_bouton_pushed = true
-        giga_bouton_activated()
-      end
-    end
-  end
-end
-
--- Déplacement de la camera le long du couloir après appui sur
--- le giga bouton d'extra collision à charge supérieure de 15.000 megawatts
-function giga_bouton_camera_move()
-
-  sol.map.camera_move(888, 792, 250, function()
-    sol.main.play_sound("secret")
-  end)
-  sol.map.sensor_set_enabled("infinite_corridor", false)
-end
-
--- Appui sur le giga bouton d'extra collision à charge supérieure de 15.000 megawatts
-function giga_bouton_activated()
-
-  sol.map.switch_set_activated("giga_bouton", true)
-  sol.map.sensor_set_enabled("infinite_corridor", false)        
-  sol.game.savegame_set_boolean(126, true)
-  giga_bouton_camera_move()
-end
-
-function event_treasure_obtained(item_name, variant, savegame_variable)
+function map:on_obtained_treasure(item_name, variant, savegame_variable)
 
   if item_name == "glove" then
     -- Fin du donjon
-    sol.map.hero_start_victory_sequence()
-    sol.main.timer_start(leave_dungeon, 2000)
+    hero:start_victory(function()
+      sol.audio.play_sound("warp")
+      hero:teleport("3", "from_crazy_house")
+    end)
   end
 end
 
-function leave_dungeon()
-
-  sol.main.play_sound("warp")
-  sol.map.hero_set_map(3, "from_crazy_house", 1)
+-- Ouverture de la barrière près du giga bouton d'extra collision à charge supérieure de 15.000 megawatts
+function barrier_switch:on_activated()
+  sol.audio.play_sound("secret")
+  map:set_entities_enabled("barrier_tile", false)
+  game:set_value("b129", true)
 end
-
-function event_switch_activated(switch_name)
-
-  -- Ouverture de la barrière près du giga bouton d'extra collision à charge supérieure de 15.000 megawatts
-  if switch_name == "barrier_switch" then
-    sol.main.play_sound("secret")
-    sol.map.tile_set_group_enabled("barrier", false)
-    sol.game.savegame_set_boolean(129, true)
-  end
-end
-
-
 
