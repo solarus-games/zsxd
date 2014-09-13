1c285c878e5ab3d731329bb4b9113a1fba374c04
Christopho <christopho@solarus-games.org>
2014-09-13 12:15:59 +0200

    Fix fairy only restoring 1/4 heart in cursed cave. Fix #40.

--- data/maps/59.lua
+++ data/maps/59.lua
@@ -9,7 +9,12 @@ end
 
 function map:on_update()
 
-  -- make sure the hero will die in the lava
+  if self:get_game():is_game_over_enabled() then
+    --- Let the fairy restore some life.
+    return
+  end
+
+  -- Make sure the hero will die in the lava
   -- even if a fairy saves him
   if self:get_game():get_life() > 1 then
     self:get_game():set_life(1)
