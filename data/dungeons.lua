82ee9d2a11ca8c7c94ba59e20ba728bf9476d7f3
Christopho <christopho@solarus-games.org>
2016-09-26 14:53:20 +0200

    Replace sol.main.load_file() by require()

--- data/dungeons.lua
+++ data/dungeons.lua
@@ -1,76 +1,77 @@
-local game = ...
+return function(game)
 
--- Define the existing dungeons and their floors for the minimap menu.
-game.dungeons = {
-  [1] = {
-    floor_width = 3072,
-    floor_height = 2048,
-    lowest_floor = 0,
-    highest_floor = 1,
-    maps = { "60", "61", "62", "63", "64", "65", "66", "67" },
-    boss = {
-      floor = 1,
-      x = 2624,
-      y = 845,
-      savegame_variable = "b306",
+  -- Define the existing dungeons and their floors for the minimap menu.
+  game.dungeons = {
+    [1] = {
+      floor_width = 3072,
+      floor_height = 2048,
+      lowest_floor = 0,
+      highest_floor = 1,
+      maps = { "60", "61", "62", "63", "64", "65", "66", "67" },
+      boss = {
+        floor = 1,
+        x = 2624,
+        y = 845,
+        savegame_variable = "b306",
+      },
     },
-  },
-  [2] = {
-    floor_width = 3056,
-    floor_height = 2064,
-    lowest_floor = 0,
-    highest_floor = 2,
-    maps = { "20", "21", "22", "23", "24" },
-  },
-}
+    [2] = {
+      floor_width = 3056,
+      floor_height = 2064,
+      lowest_floor = 0,
+      highest_floor = 2,
+      maps = { "20", "21", "22", "23", "24" },
+    },
+  }
 
--- Returns the index of the current dungeon if any, or nil.
-function game:get_dungeon_index()
+  -- Returns the index of the current dungeon if any, or nil.
+  function game:get_dungeon_index()
 
-  local world = self:get_map():get_world()
-  local index = tonumber(world:match("^dungeon_([0-9]+)$"))
-  return index
-end
-
--- Returns the current dungeon if any, or nil.
-function game:get_dungeon()
-
-  local index = self:get_dungeon_index()
-  return self.dungeons[index]
-end
-
-function game:is_dungeon_finished(dungeon_index)
-  return self:get_value("dungeon_" .. dungeon_index .. "_finished")
-end
-
-function game:set_dungeon_finished(dungeon_index, finished)
-  if finished == nil then
-    finished = true
+    local world = self:get_map():get_world()
+    local index = tonumber(world:match("^dungeon_([0-9]+)$"))
+    return index
   end
-  self:set_value("dungeon_" .. dungeon_index .. "_finished", finished)
+
+  -- Returns the current dungeon if any, or nil.
+  function game:get_dungeon()
+
+    local index = self:get_dungeon_index()
+    return self.dungeons[index]
+  end
+
+  function game:is_dungeon_finished(dungeon_index)
+    return self:get_value("dungeon_" .. dungeon_index .. "_finished")
+  end
+
+  function game:set_dungeon_finished(dungeon_index, finished)
+    if finished == nil then
+      finished = true
+    end
+    self:set_value("dungeon_" .. dungeon_index .. "_finished", finished)
+  end
+
+  function game:has_dungeon_map(dungeon_index)
+
+    dungeon_index = dungeon_index or self:get_dungeon_index()
+    return self:get_value("dungeon_" .. dungeon_index .. "_map")
+  end
+
+  function game:has_dungeon_compass(dungeon_index)
+
+    dungeon_index = dungeon_index or self:get_dungeon_index()
+    return self:get_value("dungeon_" .. dungeon_index .. "_compass")
+  end
+
+  function game:has_dungeon_big_key(dungeon_index)
+
+    dungeon_index = dungeon_index or self:get_dungeon_index()
+    return self:get_value("dungeon_" .. dungeon_index .. "_big_key")
+  end
+
+  function game:has_dungeon_boss_key(dungeon_index)
+
+    dungeon_index = dungeon_index or self:get_dungeon_index()
+    return self:get_value("dungeon_" .. dungeon_index .. "_boss_key")
+  end
+
 end
-
-function game:has_dungeon_map(dungeon_index)
-
-  dungeon_index = dungeon_index or self:get_dungeon_index()
-  return self:get_value("dungeon_" .. dungeon_index .. "_map")
-end
-
-function game:has_dungeon_compass(dungeon_index)
-
-  dungeon_index = dungeon_index or self:get_dungeon_index()
-  return self:get_value("dungeon_" .. dungeon_index .. "_compass")
-end
-
-function game:has_dungeon_big_key(dungeon_index)
-
-  dungeon_index = dungeon_index or self:get_dungeon_index()
-  return self:get_value("dungeon_" .. dungeon_index .. "_big_key")
-end
-
-function game:has_dungeon_boss_key(dungeon_index)
-
-  dungeon_index = dungeon_index or self:get_dungeon_index()
-  return self:get_value("dungeon_" .. dungeon_index .. "_boss_key")
-end
-
--- data/play_game.lua
+++ data/play_game.lua
@@ -1,78 +1,79 @@
-local game = ...
+return function(game)
 
--- Include the various game features.
-sol.main.load_file("dungeons")(game)
-sol.main.load_file("equipment")(game)
-sol.main.load_file("menus/pause")(game)
-sol.main.load_file("menus/dialog_box")(game)
-sol.main.load_file("menus/game_over")(game)
-sol.main.load_file("hud/hud")(game)
+  -- Include the various game features.
+  require("dungeons")(game)
+  require("equipment")(game)
+  require("menus/pause")(game)
+  require("menus/dialog_box")(game)
+  require("menus/game_over")(game)
+  require("hud/hud")(game)
 
--- Useful functions for this specific quest.
+  -- Useful functions for this specific quest.
 
-function game:on_started()
+  function game:on_started()
 
-  -- Set up the dialog box and the HUD.
-  self:initialize_dialog_box()
-  self:initialize_hud()
+    -- Set up the dialog box and the HUD.
+    self:initialize_dialog_box()
+    self:initialize_hud()
+  end
+
+  function game:on_finished()
+
+    -- Clean what was created by on_started().
+    self:quit_hud()
+    self:quit_dialog_box()
+  end
+
+  -- This event is called when a new map has just become active.
+  function game:on_map_changed(map)
+
+    -- Notify the hud.
+    self:hud_on_map_changed(map)
+  end
+
+  function game:on_paused()
+    self:hud_on_paused()
+    self:start_pause_menu()
+  end
+
+  function game:on_unpaused()
+    self:stop_pause_menu()
+    self:hud_on_unpaused()
+  end
+
+  function game:get_player_name()
+    return self:get_value("player_name")
+  end
+
+  function game:set_player_name(player_name)
+    self:set_value("player_name", player_name)
+  end
+
+  -- Returns whether the current map is in the inside world.
+  function game:is_in_inside_world()
+    return self:get_map():get_world() == "inside_world"
+  end
+
+  -- Returns whether the current map is in the outside world.
+  function game:is_in_outside_world()
+    return self:get_map():get_world() == "outside_world"
+  end
+
+  -- Returns whether the current map is in a dungeon.
+  function game:is_in_dungeon()
+    return self:get_dungeon() ~= nil
+  end
+
+  -- Allows the hero to jump over water when he cannot swim.
+  -- This was the default behavior before Solarus 1.5.
+  local function fix_jump_over_water(game)
+
+    game:set_ability("jump_over_water", 1)
+  end
+
+  -- Run the game.
+  sol.main.game = game
+  fix_jump_over_water(game)
+  game:start()
+
 end
-
-function game:on_finished()
-
-  -- Clean what was created by on_started().
-  self:quit_hud()
-  self:quit_dialog_box()
-end
-
--- This event is called when a new map has just become active.
-function game:on_map_changed(map)
-
-  -- Notify the hud.
-  self:hud_on_map_changed(map)
-end
-
-function game:on_paused()
-  self:hud_on_paused()
-  self:start_pause_menu()
-end
-
-function game:on_unpaused()
-  self:stop_pause_menu()
-  self:hud_on_unpaused()
-end
-
-function game:get_player_name()
-  return self:get_value("player_name")
-end
-
-function game:set_player_name(player_name)
-  self:set_value("player_name", player_name)
-end
-
--- Returns whether the current map is in the inside world.
-function game:is_in_inside_world()
-  return self:get_map():get_world() == "inside_world"
-end
-
--- Returns whether the current map is in the outside world.
-function game:is_in_outside_world()
-  return self:get_map():get_world() == "outside_world"
-end
-
--- Returns whether the current map is in a dungeon.
-function game:is_in_dungeon()
-  return self:get_dungeon() ~= nil
-end
-
--- Allows the hero to jump over water when he cannot swim.
--- This was the default behavior before Solarus 1.5.
-local function fix_jump_over_water(game)
-
-  game:set_ability("jump_over_water", 1)
-end
-
--- Run the game.
-sol.main.game = game
-fix_jump_over_water(game)
-game:start()
-
